73d271c4bd5de1f05c61daf2ca53377fd60ac40a
Jonathan Aherne <jonathanaherne@gmail.com>
2024-10-05 16:38:03 -0700

    Make pattern required

--- src/main.rs
+++ src/main.rs
@@ -11,35 +11,29 @@ enum Input {
 
 fn main() {
 
-    let m = Command::new("fml")
+    let mut cmd = Command::new("fml")
         .author("Jonathan Aherne, jonathan@elevenchars.com")
         .version("0.1.0")
         .about("Highlights lines in a file or stdin that match a pattern")
-        .arg(Arg::new("pattern").help("The search pattern").index(1))
-        .arg(Arg::new("file").help("The input file (optional)").index(2).required(false))
-        .get_matches();
+        .arg(Arg::new("pattern").help("The search pattern").index(1).required(true))
+        .arg(Arg::new("file").help("The input file (optional)").index(2));
+        // Todo: add option for additional color schemes
+        // Todo: add option for case sensitivity
+        // Todo: add option for regex
+    let m = cmd.get_matches_mut();
 
     let pattern = m.get_one::<String>("pattern").expect("Pattern is required");
     println!("Value for pattern: {pattern}");
     let file = m.get_one::<String>("file");
-    if file.is_some() {
-        let input = file.unwrap();
-        println!("Value for input file: {input}");
-    } else {
-        let _input = std::io::stdin();
-        println!("Reading from stdin");
-    }
 
-    // Define the reader
     let reader: Input;
 
     // Open the file, assuming we have one
     if file.is_some() {
+        println!("Opening file: {}", file.unwrap());
         let file = std::fs::File::open(file.unwrap()).expect("Could not open file");
         reader = Input::File(BufReader::new(file));
-    }
-    // Open stdin if we don't have a file
-    else {
+    } else { // Open stdin as we don't have a file
         reader = Input::Stdin(BufReader::new(std::io::stdin()));
     }
 
@@ -49,15 +43,18 @@ fn main() {
     }
 }
 
+// Process the input reader agnostic, highlighting the pattern if found
 fn process<R: BufRead>(reader: R, pattern: &str) {
     for line in reader.lines() {
         let line = line.expect("Could not read line");
         if line.contains(pattern) {
             // Get the index of the pattern in the line
+            // Todo: can we combine find and contains?
             let start = line.find(pattern).expect("Pattern not found");
             let end = start + pattern.len();
 
             // Print the line with the pattern highlighted
+            // Todo: check if this works with wide characters. Probably not
             print!("{}", &line[..start]);
             print!("{}", &line[start..end].bold().red());
             println!("{}", &line[end..]);
